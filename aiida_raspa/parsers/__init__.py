963af1ef10bb1cb807158dc1fdb63a6489c98b17
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2018-01-24 16:06:18 +0100

    Get number of absorbed molecules from *data file
    1. Plugin is able to correctly identify *data file
    2. For the moment the parsing of the file is rather simple: output plugin only
    takes the last number of the absorbed molecules from the *.data file

--- aiida_raspa/parsers/__init__.py
+++ aiida_raspa/parsers/__init__.py
@@ -35,14 +35,18 @@ class RaspaParser(Parser):
         return True, new_nodes_list
 
     def _parse_stdout(self, out_folder, new_nodes_list):
-        fn = self._calc._OUTPUT_FILE_NAME
+        #fn = self._calc._OUTPUT_FILE_NAME
+        fs = out_folder.get_folder_list()
+        for f in fs:
+            if f.endswith('.data'):
+                fn=f
 
         result_dict = {'exceeded_walltime': False}
-#        abs_fn = out_folder.get_abs_path(fn)
-#        with open(abs_fn, "r") as f:
-#            for line in f.readlines():
-#                if line.startswith(' ENERGY| '):
-#                    result_dict['energy'] = float(line.split()[8])
+        abs_fn = out_folder.get_abs_path(fn)
+        with open(abs_fn, "r") as f:
+            for line in f.readlines():
+                if line.startswith('Number of Adsorbates:'):
+                    result_dict['number_of_adsorbates'] = float(line.split()[3])
 #                    result_dict['energy_units'] = "a.u."
 #                if 'The number of warnings for this run is' in line:
 #                    result_dict['nwarnings'] = int(line.split()[-1])
