1af3ae81a454a550ae98c8909f74ff85dc7ea565
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2018-02-09 23:20:30 +0100

    Enable parsing of multicomponent system

--- aiida_raspa/parsers/__init__.py
+++ aiida_raspa/parsers/__init__.py
@@ -8,7 +8,6 @@ from aiida_raspa.calculations import RaspaCalculation
 
 def block_analysis(fl,value=1,units=2,dev=4):
     for line in fl:
-        print line,
         if 'Average' in line:
             break
     return (float(line.split()[value]), line.split()[units][1:-1],
@@ -18,7 +17,7 @@ class RaspaParser(Parser):
     """
     Parser for the output of RASPA.
     """
-    _linkname_outparams = "component_0"
+#    _linkname_outparams = "component_0"
 
     # --------------------------------------------------------------------------
     def __init__(self, calc):
@@ -46,9 +45,15 @@ class RaspaParser(Parser):
     def _parse_stdout(self, out_folder, new_nodes_list):
         #fn = self._calc._OUTPUT_FILE_NAME
         fs = out_folder.get_folder_list()
+        component = []
         for f in fs:
             if f.endswith('.data'):
                 fn=f
+        
+        inp_params = self._calc.inp.parameters.get_dict()
+        for i in range(len(inp_params['Component'])):
+            component.append({})
+
 
         result_dict = {'exceeded_walltime': False}
         abs_fn = out_folder.get_abs_path(fn)
@@ -56,59 +61,69 @@ class RaspaParser(Parser):
         av_pressure = re.compile("Average Pressure:")
         av_temperature = re.compile("Average temperature:")
         av_density = re.compile("Average Density:")
-        av_heat_of_desorpt= re.compile("Heat of desorption:")
-        av_tot_energy= re.compile("Total energy:")
+        av_heat_of_desorpt = re.compile("Heat of desorption:")
+        av_tot_energy = re.compile("Total energy:$")
+        num_of_molec = re.compile("Number of molecules:$")
         result_dict = {'exceeded_walltime': False}
         with open(abs_fn, "r") as f:
             for line in f:
                 if 'Finishing simulation' in line:
                     break
             for line in f:
-                if 'Average loading excess [molecules/unit cell]' in line:
-                    result_dict['loading_absolute_average'] = float(line.split()[5])
-                    result_dict['loading_absolute_dev'] = float(line.split()[7])
-                    result_dict['loading_absolute_units'] = 'molecules/unit cell'
-    
                 if av_volume.match(line) is not None:
                     (result_dict['cell_volume_average'],
                     result_dict['cell_volume_units'],
                     result_dict['cell_volume_dev']) =  block_analysis(f)
+                    continue
     
                 if av_pressure.match(line) is not None:
                     (result_dict['pressure_average'],
                     result_dict['pressure_units'],
                     result_dict['pressure_dev']) =  block_analysis(f)
+                    continue
     
                 if av_temperature.match(line) is not None:
                     (result_dict['temperature_average'],
                     result_dict['temperature_units'],
                     result_dict['temperature_dev']) =  block_analysis(f)
+                    continue
     
                 if av_density.match(line) is not None:
                     (result_dict['density_average'],
                     result_dict['density_units'],
                     result_dict['density_dev']) =  block_analysis(f)
+                    continue
     
                 if av_heat_of_desorpt.match(line) is not None:
                     (result_dict['heat_of_desorption_average'],
                     result_dict['heat_of_desorption_units'],
                     result_dict['heat_of_desorption_dev']) = \
                     block_analysis(f,units=4,dev=3)
+                    continue
     
-                if av_heat_of_desorpt.match(line) is not None:
+                if av_tot_energy.match(line) is not None:
                     (result_dict['total_energy_average'],
                     result_dict['total_energy_units'],
                     result_dict['total_energy_dev']) = \
-                    block_analysis(f,units=4,dev=3)
-               
-    
-                if 'warnings' in line:
-                    result_dict['nwarnings'] = int(line.split()[-2])
-    
+                    block_analysis(f)
+                    continue
+                
+                if num_of_molec.match(line) is not None:
+                    break
+#                if 'warnings' in line:
+#                    result_dict['nwarnings'] = int(line.split()[-2])
+            i = 0
+            for line in f:
+                if 'Average loading excess [molecules/unit cell]' in line:
+                    component[i]['loading_absolute_average'] = float(line.split()[5])
+                    component[i]['loading_absolute_dev'] = float(line.split()[7])
+                    component[i]['loading_absolute_units'] = 'molecules/unit cell'
+                    i+=1
 #                if 'exceeded requested execution time' in line:
 #                    result_dict['exceeded_walltime'] = True
 
         pair = (self.get_linkname_outparams(), ParameterData(dict=result_dict))
         new_nodes_list.append(pair)
-        pair2 = ('component_1', ParameterData(dict={'test':1}))
-        new_nodes_list.append(pair2)
+        for i, item in enumerate(component):
+            pair = ('component_'+str(i), ParameterData(dict=item))
+            new_nodes_list.append(pair)
