a6827cce9a4472f327dfab219c1a9f148aeb0618
Daniele Ongari <daniele.ongari@gmail.com>
2019-05-25 00:20:19 +0200

    fixed perpendicular width

--- aiida_raspa/workflows/__init__.py
+++ aiida_raspa/workflows/__init__.py
@@ -28,28 +28,25 @@ default_options = {
     "max_wallclock_seconds": 1 * 60 * 60,
 }
 
-
 # pylint: disable=too-many-locals
 def multiply_unit_cell(cif, threshold):
     """Returns the multiplication factors (tuple of 3 int) for the cell vectors
     to respect, in every direction: min(perpendicular_width) > threshold
     """
-    from math import cos, sin, sqrt, pi
+    from math import cos, sin, sqrt, pi, fabs, ceil
     import numpy as np
     deg2rad = pi / 180.
 
+    # Parsing cif
     struct = next(six.itervalues(cif.values.dictionary))
-
     a = float(struct['_cell_length_a'])
     b = float(struct['_cell_length_b'])
     c = float(struct['_cell_length_c'])
-
     alpha = float(struct['_cell_angle_alpha']) * deg2rad
     beta = float(struct['_cell_angle_beta']) * deg2rad
     gamma = float(struct['_cell_angle_gamma']) * deg2rad
 
-    # first step is computing cell parameters according to  https://en.wikipedia.org/wiki/Fractional_coordinates
-    # Note: this is the algorithm implemented in Raspa (framework.c/UnitCellBox). There also is a simpler one but it is less robust.
+    # Computing triangular cell matrix
     v = sqrt(1 - cos(alpha)**2 - cos(beta)**2 - cos(gamma)**2 +
              2 * cos(alpha) * cos(beta) * cos(gamma))
     cell = np.zeros((3, 3))
@@ -62,10 +59,20 @@ def multiply_unit_cell(cif, threshold):
     ]
     cell = np.array(cell)
 
-    # diagonalizing the cell matrix: note that the diagonal elements are the perpendicolar widths because ay=az=bz=0
-    diag = np.diag(cell)
-    return tuple(int(i) for i in np.ceil(threshold / diag * 2.))
+    # Computing perpendicular widths, as implemented in Raspa
+    # for the check (simplified for triangular cell matrix)
+    axc1 = cell[0,0] * cell[2,2]
+    axc2 = - cell[0,0] * cell[2,1]
+    bxc1 = cell[1,1] * cell[2,2]
+    bxc2 = - cell[1,0] * cell[2,2]
+    bxc3 = cell[1,0] * cell[2,1] - cell[1,1] * cell[2,0]
+    det = fabs(cell[0,0] * cell[1,1] * cell[2,2])
+    perpwidth = np.zeros(3)
+    perpwidth[0] = det / sqrt(bxc1**2 + bxc2**2 + bxc3**2)
+    perpwidth[1] = det / sqrt(axc1**2 + axc2**2)
+    perpwidth[2] = cell[2,2]
 
+    return tuple(int(ceil(threshold / perpwidth[i])) for i in range(3))
 
 class RaspaConvergeWorkChain(WorkChain):
     """A base workchain to get converged RASPA calculations"""
