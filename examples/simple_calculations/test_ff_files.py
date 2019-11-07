b7aff717a08a98b42d2160eb1f83e283c948690f
Daniele Ongari <daniele.ongari@gmail.com>
2019-11-07 15:43:17 +0100

    add test files

---
+++ examples/simple_calculations/test_ff_files.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""Run RASPA calculation using Local force field"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+import sys
+import os
+import click
+
+from aiida.common import NotExistent
+from aiida.engine import run_get_pk, run
+from aiida.orm import Code, Dict, CifData, SinglefileData
+
+
+@click.command('cli')
+@click.argument('codelabel')
+@click.option('--submit', is_flag=True, help='Actually submit calculation')
+def main(codelabel, submit):
+    """Prepare and submit RASPA calculation with components mixture."""
+    try:
+        code = Code.get_from_string(codelabel)
+    except NotExistent:
+        print("The code '{}' does not exist".format(codelabel))
+        sys.exit(1)
+
+    # parameters
+    parameters = Dict(
+        dict={
+            "GeneralSettings": {
+                "SimulationType": "MonteCarlo",
+                "NumberOfInitializationCycles": 200,
+                "NumberOfCycles": 300,
+                "PrintEvery": 100,
+                "Forcefield": "Local",
+                "ChargeMethod": "Ewald",
+                "EwaldPrecision": 1e-6,
+                "CutOff": 12.0,
+            },
+            "System": {
+                "irmof_1": {
+                    "type": "Framework",
+                    "UnitCells": "1 1 1",
+                    "HeliumVoidFraction": 0.149,
+                    "ExternalTemperature": 300.0,
+                    "ExternalPressure": 1e5,
+                }
+            },
+            "Component": {
+                "CO2": {
+                    "MoleculeDefinition": "Local",
+                    "MolFraction": 0.30,
+                    "TranslationProbability": 1.0,
+                    "RotationProbability": 1.0,
+                    "ReinsertionProbability": 1.0,
+                    "SwapProbability": 1.0,
+                    "CreateNumberOfMolecules": 0,
+                },
+                "N2": {
+                    "MoleculeDefinition": "Local",
+                    "MolFraction": 0.70,
+                    "TranslationProbability": 1.0,
+                    "RotationProbability": 1.0,
+                    "ReinsertionProbability": 1.0,
+                    "SwapProbability": 1.0,
+                    "CreateNumberOfMolecules": 0,
+                },
+            },
+        })
+
+    # Contructing builder
+    builder = code.get_builder()
+    builder.framework = {
+        "irmof_1": CifData(file=os.path.abspath('files/IRMOF-1_eqeq.cif')),
+    }
+    builder.file = {
+        "file_1": SinglefileData(file=os.path.abspath('files/force_field_mixing_rules.def')),
+        "file_2": SinglefileData(file=os.path.abspath('files/pseudo_atoms.def')),
+        "file_3": SinglefileData(file=os.path.abspath('files/CO2.def')),
+        "file_4": SinglefileData(file=os.path.abspath('files/N2.def')),
+    }
+    builder.parameters = parameters
+    builder.metadata.options = {
+        "resources": {
+            "num_machines": 1,
+            "num_mpiprocs_per_machine": 1,
+        },
+        "max_wallclock_seconds": 1 * 30 * 60,  # 30 min
+        "withmpi": False,
+    }
+    builder.metadata.dry_run = False
+    builder.metadata.store_provenance = True
+
+    if submit:
+        print("Testing RASPA CO2/N2 adsorption in IRMOF-1, using Local force field ...")
+        res, pk = run_get_pk(builder)
+        print("calculation pk: ", pk)
+        print("CO2/N2 uptake ({:s}): {:.2f}/{:.2f} ".format(
+            res['output_parameters']["irmof_1"]["components"]['N2']["loading_absolute_unit"],
+            res['output_parameters']["irmof_1"]["components"]['CO2']["loading_absolute_average"],
+            res['output_parameters']["irmof_1"]["components"]['N2']["loading_absolute_average"],
+        ))
+        print("OK, calculation has completed successfully")
+    else:
+        print("Generating test input ...")
+        builder.metadata.dry_run = True
+        builder.metadata.store_provenance = False
+        run(builder)
+        print("Submission test successful")
+        print("In order to actually submit, add '--submit'")
+    print("-----")
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
+
+# EOF
