ece7bb5990e9b22152ec5a7b416b65885687573f
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-02-10 11:22:47 +0100

    chwon -R

--- .docker/my_init.d/add-codes.sh
+++ .docker/my_init.d/add-codes.sh
@@ -4,4 +4,4 @@ set -em
 su -c /opt/add-codes.sh ${SYSTEM_USER}
 
 # Make /opt/aiida-raspa folder editable for the $SYSTEM_USER.
-chown ${SYSTEM_USER}:${SYSTEM_USER} /opt/aiida-raspa/
+chown -R ${SYSTEM_USER}:${SYSTEM_USER} /opt/aiida-raspa/
